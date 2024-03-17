bb0765e8e875423aff808d10d86672608eecaec3
Hassaan Arain <161812835+MuhammadHassaanArain@users.noreply.github.com>
2024-03-17 01:43:24 -0700

    Add files via upload

---
+++ Question 04 ; Famous Quote/main.js
@@ -0,0 +1,12 @@
+"use strict";
+/*Questin # 4 : Famous Qoute:
+                Find a quote from a famous person you admire.
+                print the quote and the name of its author.
+                your output should look something like the following,
+                including the quotation marks:
+        Albert Einstein once said,"A person who never made a mistake
+                                   never tried anything new "   */
+Object.defineProperty(exports, "__esModule", { value: true });
+let famouspersonname = "Charlie Chaplin";
+console.log(`${famouspersonname} once said,"You will never find rainbows if 
+you are looking down."`);
---
+++ Question 04 ; Famous Quote/main.ts
@@ -0,0 +1,11 @@
+/*Questin # 4 : Famous Qoute:
+                Find a quote from a famous person you admire.
+                print the quote and the name of its author.
+                your output should look something like the following,
+                including the quotation marks:
+        Albert Einstein once said,"A person who never made a mistake 
+                                   never tried anything new "   */
+
+let famouspersonname : string ="Charlie Chaplin";
+console.log(`${famouspersonname} once said,"You will never find rainbows if 
+you are looking down."`)
