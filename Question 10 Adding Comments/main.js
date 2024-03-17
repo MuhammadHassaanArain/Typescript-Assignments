8afd84cee059e89963a7ba7c62b96cace96cfae8
Hassaan Arain <161812835+MuhammadHassaanArain@users.noreply.github.com>
2024-03-17 09:30:01 -0700

    Add files via upload

---
+++ Question 10 Adding Comments/main.js
@@ -0,0 +1,15 @@
+"use strict";
+/* Question #10; Adding Comments:
+                 Choose two of the program you have written,and add
+                 at least one comment to each.If you do not have anything
+                 specific to write because your programs are too simple at
+                 this point,just add  your name and the current date at the
+                 top of each program file.Then write one sentence describing
+                 what the program does.
+  */
+Object.defineProperty(exports, "__esModule", { value: true });
+// Hassaan Arain,  Dated 17|march|2024.
+// it's a variable and the its type is string.
+let myName = "Hassaan Arain";
+// Here printing variable 
+console.log(`This is ${myName} from hyderabad sindh.`);
---
+++ Question 10 Adding Comments/main.ts
@@ -0,0 +1,15 @@
+/* Question #10; Adding Comments:
+                 Choose two of the program you have written,and add 
+                 at least one comment to each.If you do not have anything
+                 specific to write because your programs are too simple at 
+                 this point,just add  your name and the current date at the 
+                 top of each program file.Then write one sentence describing 
+                 what the program does.
+  */
+
+// Hassaan Arain,  Dated 17|march|2024.
+// it's a variable and the its type is string.
+ let myName : string = "Hassaan Arain";
+// Here printing variable 
+  console.log(`This is ${myName} from hyderabad sindh.`);
+ 
