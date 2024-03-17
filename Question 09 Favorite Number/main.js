a497b3f39ed2e6358d99c7798188d296995a7093
Hassaan Arain <161812835+MuhammadHassaanArain@users.noreply.github.com>
2024-03-17 08:54:31 -0700

    Add files via upload

---
+++ Question 09 Favorite Number/main.js
@@ -0,0 +1,10 @@
+"use strict";
+/* Question # 9; Favorite Number:
+                 store your favorite number in variable.Then,using that
+                 variable,create a message that reveals your favorite number.
+                 print that message.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+let favoriteNumber = 8;
+let message = `My favorite number is ${favoriteNumber}.Because it's my Birth date.`;
+console.log(message);
---
+++ Question 09 Favorite Number/main.ts
@@ -0,0 +1,9 @@
+/* Question # 9; Favorite Number:
+                 store your favorite number in variable.Then,using that
+                 variable,create a message that reveals your favorite number.
+                 print that message.
+ */
+
+                 let favoriteNumber : number = 8;
+                 let message : string = `My favorite number is ${favoriteNumber}.Because it's my Birth date.`;
+                 console.log(message);
