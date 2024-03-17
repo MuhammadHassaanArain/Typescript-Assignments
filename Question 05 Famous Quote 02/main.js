e366874e27a38fe58efc709b1f894e51cd5e66c6
Hassaan Arain <161812835+MuhammadHassaanArain@users.noreply.github.com>
2024-03-17 02:36:50 -0700

    Add files via upload

---
+++ Question 05 Famous Quote 02/main.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/* Question # 5; Famous Quote 2 :
+repeat exercise 4,but this time store the famous person's name
+in a variable called famous_person. Then compose you message and
+store it in a new variable called messsage.Print your message.
+ */
+let famous_person = "Benjamin Franklin";
+let message = "Three can keep a secret, if two of them are dead.";
+console.log(`${famous_person} once said, "${message}"`);
---
+++ Question 05 Famous Quote 02/main.ts
@@ -0,0 +1,8 @@
+/* Question # 5; Famous Quote 2 :
+repeat exercise 4,but this time store the famous person's name 
+in a variable called famous_person. Then compose you message and 
+store it in a new variable called messsage.Print your message. 
+ */
+let famous_person : string = "Benjamin Franklin";
+let message : string = "Three can keep a secret, if two of them are dead.";
+console.log(`${famous_person} once said, "${message}"`);
