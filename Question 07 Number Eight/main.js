2815c8eb6ed21931349a964bdde96449cb71c539
Hassaan Arain <161812835+MuhammadHassaanArain@users.noreply.github.com>
2024-03-17 08:05:36 -0700

    Add files via upload

---
+++ Question 07 Number Eight/main.js
@@ -0,0 +1,11 @@
+"use strict";
+/* Question # 7; Number Eight:
+                 write addition,substraction,multiplication,and division operations that
+                 each result in the number 8.Be sure to enclose your operators in print statements
+                 to see the results.
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log(3 + 5); // Addition.
+console.log(54 - 46); //substraction.
+console.log(2 * 4); // Multiplication.
+console.log(16 / 2); //Dvision.
