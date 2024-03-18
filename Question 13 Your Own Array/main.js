d43819a78edb51e8e0402124e9d3910bd8a701a0
Hassaan Arain <161812835+MuhammadHassaanArain@users.noreply.github.com>
2024-03-18 08:51:41 -0700

    Add files via upload

---
+++ Question 13 Your Own Array/main.js
@@ -0,0 +1,16 @@
+"use strict";
+// /* Question#13; Your Own Array:
+//                 Think of your Favorite mode of transportation,such as a
+//                 motorcycle or a car,and make a list that stores several examples.
+//                 use your list to print a series of statements about these items,
+//                 such as "I would like to own a Honda motorcycle."
+// */
+Object.defineProperty(exports, "__esModule", { value: true });
+let vehicles_list = [
+    "Hyundai sonata",
+    "Porsche Taycan",
+    "Toyota Revo",
+];
+vehicles_list.forEach((vehicles_list) => {
+    console.log(`I would like to own a ${vehicles_list}.`);
+});
