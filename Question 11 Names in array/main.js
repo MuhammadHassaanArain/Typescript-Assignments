3b5146357c3a5a8139534711e46bc39b9d67d8a5
Hassaan Arain <161812835+MuhammadHassaanArain@users.noreply.github.com>
2024-03-18 04:34:34 -0700

    Add files via upload

---
+++ Question 11 Names in array/main.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/* Question#11; Names in array;
+                Store the names of a few of your friends in a array called names.
+                print each person's name by accessing each element in the list,
+                one at a time.
+ */
+let names = ["Farhan Mughal", "Sarim Sheikh", "Abrar Ali"];
+for (let index = 0; index < names.length; index++) {
+    console.log(names[index]);
+}
